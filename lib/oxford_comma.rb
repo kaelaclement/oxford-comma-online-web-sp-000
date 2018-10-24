dded6690c5ad5b2709d94770eff4de2665e581fa
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-24 21:37:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,15 @@
 def oxford_comma(array)
-  sentence = array.join(", ")
-  index = sentence.length - 2
-  sentence[index] << "and "
-  sentence
+  if array.size == 1
+    array
+  elsif array.size == 2
+    array.insert(1, "and")
+    array.join(" ")
+  end
+  
+  index = array.size - 2
+  array.insert(index, "and")
+# iterate to add commas & and
+
+# then join
+
 end
