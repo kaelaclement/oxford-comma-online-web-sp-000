b7eeb5d4c27f69a2a4beb77696e84bf4e05c578a
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-24 21:47:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,13 @@
 def oxford_comma(array)
   if array.size == 1
-    array.join
+    array[0]
   elsif array.size == 2
-    array.insert(1, "and")
-    array.join(" ")
+    array.join(" and ")
   end
-
   array.each do |word|
+    if array.size == 2
+
+
   end
 # iterate to add commas & and
 
